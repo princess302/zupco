7f175a4f01c186ed637e32f08b505240884325eb
Princess <r203216e@students.msu.ac.zw>
2024-05-02 12:17:24 +0200

    Your commit message

--- did_django_google_api_tutorial-main/vehicles/views.py
+++ did_django_google_api_tutorial-main/vehicles/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Vehicle
+from .serializers import VehicleSerializer
 
-# Create your views here.
+class VehicleList(APIView):
+    def get(self, request):
+        vehicles = Vehicle.objects.all()
+        serializer = VehicleSerializer(vehicles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = VehicleSerializer(data=request.data)
+
+    def put(self, request, pk):
+        vehicle = get_object_or_404(Vehicle, pk=pk)
+        serializer = VehicleSerializer(vehicle, data=request.data)
+        
+        if serializer.is_valid():
+            # Add your code here
+            pass
